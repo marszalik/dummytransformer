2f947f4828575e16e9453173a776cd3ba1a0aba4
agodlewski <arturasg1@gmail.com>
2024-11-28 12:42:24 +0100

    examples for embeddings and positionalEncoding added

--- EmbeddingsTest.py
+++ EmbeddingsTest.py
@@ -3,5 +3,7 @@
 from Embeddings import Embeddings
 
 if __name__ == "__main__":
-    embeddings = Embeddings("embeddings.json")
-    print("Embedding for 'Ala':", embeddings.get_embedding("Ala"))  # Wyświetli: [0.12, 0.85, -0.34]
+    embeddings = Embeddings()
+    word = "dog"
+    vector = embeddings.get_embedding(word)
+    print("Vector for 'dog':", vector)  # Wyświetli: [0.45, 0.01, -0.56, 0.55]
